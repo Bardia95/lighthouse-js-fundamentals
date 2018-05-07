abb00468cef6516afc0be79405759dff74384bdf
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-05-07 22:43:12 +0000

    new functions

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var item = 0;
+while (item < ingredients.length) {
+  console.log(ingredients[item]);
+  item++;
+}
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (var b = ingredients.length - 1; b >= 0; b--) {
+  console.log(incredients[b]);
+}
